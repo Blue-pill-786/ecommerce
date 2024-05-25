a77be80652adf4eaf18878622c9e9df2b1fa1e04
Blue-pill-786 <113605539+Blue-pill-786@users.noreply.github.com>
2024-05-25 18:18:22 +0530

    add product and delete product

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar } from './Navbar.js';
 import { Products } from './Products';
+import { Link } from 'react-router-dom';
 
 
 export const Home = () => {
@@ -12,6 +13,23 @@ export const Home = () => {
         <>
             <Navbar  /> 
             <Products />
+            <div>
+                <h1>
+                    <Link to='/addproducts'>
+
+                    Add Products
+                    </Link>
+                    </h1>
+                <h1>
+                    <Link to='/delete'>
+
+                    Delete Products
+                    </Link>
+
+                </h1>
+
+
+            </div>
         </>
     );
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './context/UserContext';
 import 'react-toastify/dist/ReactToastify.css'; // Import Toastify CSS
 import { Provider } from 'react-redux';
 import store from './store';
@@ -11,10 +10,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
-    <UserProvider>
+   
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </UserProvider>
+   
   </Provider>
 );
